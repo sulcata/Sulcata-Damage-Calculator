3e80f212e9596692b3e8c4dd8ff04e1db0ac7b9f
sulcata <sulcata@users.noreply.github.com>
2017-04-10 07:53:35 -0400

    Update dependencies. Migrate to vue-i18n v6.x.

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import {i18n} from "./mixins/translation";
 
 // eslint-disable-next-line no-new
 new Vue({
     el: "#app",
-    render: h => h(App)
+    render: h => h(App),
+    i18n
 });
