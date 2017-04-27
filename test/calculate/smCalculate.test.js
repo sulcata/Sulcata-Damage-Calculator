290c0b42cc56a936bb3a5cca22a71584b0311e6d
sulcata <sulcata@users.noreply.github.com>
2017-04-27 18:18:37 -0400

    Add Aurora Veil

--- test/calculate/smCalculate.test.js
+++ test/calculate/smCalculate.test.js
@@ -8,29 +8,52 @@ const {max} = Math;
 
 describe("smCalculate()", () => {
     let field;
+    let landorusT;
+    let heatran;
+    let lavaPlume;
+    let earthquake;
 
     beforeEach(() => {
         field = new Field({gen: Gens.SM});
-    });
-
-    test("sanity check", () => {
-        const attacker = new Pokemon({
+        heatran = new Pokemon({
             name: "Heatran",
             gen: Gens.SM,
             evs: [252, 0, 0, 0, 4, 252],
             natureName: "Timid"
         });
-        const defender = new Pokemon({
+        landorusT = new Pokemon({
             name: "Landorus-Therian",
             gen: Gens.SM,
             evs: [252, 0, 216, 0, 24, 16],
             natureName: "Impish"
         });
-        const move = new Move({
+        lavaPlume = new Move({
             name: "Lava Plume",
             gen: Gens.SM
         });
-        const damage = smCalculate(attacker, defender, move, field);
+        earthquake = new Move({
+            name: "Earthquake",
+            gen: Gens.SM
+        });
+    });
+
+    test("sanity check", () => {
+        const damage = smCalculate(heatran, landorusT, lavaPlume, field);
         expect(max(...damage)).toEqual(150);
+    });
+
+    test("Aurora Veil", () => {
+        heatran.auroraVeil = true;
+        const damageSingle = smCalculate(landorusT, heatran, earthquake, field);
+        expect(damageSingle).toEqual([
+            284, 288, 290, 294, 296, 300, 302, 308,
+            312, 314, 318, 320, 324, 326, 330, 336
+        ]);
+        field.multiBattle = true;
+        const damageMulti = smCalculate(landorusT, heatran, earthquake, field);
+        expect(damageMulti).toEqual([
+            283, 288, 291, 291, 296, 299, 304, 307,
+            312, 312, 315, 320, 323, 328, 331, 336
+        ]);
     });
 });
