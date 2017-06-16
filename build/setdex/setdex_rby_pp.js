f5136877f390576e74b6cbe6af36cff0533fb6b9
DisasterAreaPS <gsalomon@zoho.com>
2017-06-16 03:13:30 +0100

    Create setdex_rby_pp.js (#7)
    * Create setdex_rby_pp.js
    
    * Update setdex_rby_pp.js

---
+++ build/setdex/setdex_rby_pp.js
@@ -0,0 +1,382 @@
+export default {
+	"Abra": {
+		"4U Standard": {"level":100,"moves":["Psychic","Seismic Toss","Thunder Wave","Counter"]},
+	},
+	{
+	"Aerodactyl": {
+		"3U Standard": {"level":100,"moves":["Double Edge","Hyper Beam","Fire Blast","Sky Attack"]},
+	},
+	{
+	"Arbok": {
+		"5U Standard": {"level":100,"moves":["Glare","Wrap","Earthquake","Hyper Beam"]},
+	},
+	{
+	"Arcanine": {
+		"4U Standard": {"level":100,"moves":["Fire Blast","Body Slam","Hyper Beam","Rest"]},
+	},
+	{
+	"Alakazam": {
+		"1U Standard": {"level":100,"moves":["Psychic","Recover","Thunder Wave","Seismic Toss"]},
+	},
+	{
+	"Articuno": {
+		"1U Standard": {"level":100,"moves":["Blizzard","Hyper Beam","Agility","Double Edge"]},
+		"1U Reflect + Rest": {"level":100,"moves":["Blizzard","Hyper Beam","Reflect","Rest"]},
+	},
+	{
+	"Blastoise": {
+		"3U Standard": {"level":100,"moves":["Surf","Blizzard","Body Slam","Rest"]},
+	},
+	{
+	"Butterfree": {
+		"6U Standard": {"level":100,"moves":["Sleep Powder","Stun Spore","Psychic","Mega Drain"]},
+	},
+	{
+	"Chansey": {
+		"1U Standard": {"level":100,"moves":["Soft-Boiled","Ice Beam","Thunder Wave","Thunderbolt"]},
+		"1U Reflect + Seismic Toss (Raish Chansey)": {"level":100,"moves":["Soft-Boiled","Seismic Toss","Thunder Wave","Reflect"]},
+		"1U Sing + Seismic Toss (Dedicated Status Platform)": {"level":100,"moves":["Soft-Boiled","Seismic Toss","Thunder Wave","Sing"]},
+	},
+	{
+	"Charizard": {
+		"3U Mixed Attacker": {"level":100,"moves":["Fire Blast","Fire Spin","Slash","Earthquake"]},
+		"3U Swords Dance": {"level":100,"moves":["Fire Blast","Swords Dance","Hyper Beam","Mega Kick"]},
+	},
+	{
+	"Clefable": {
+		"3U Standard": {"level":100,"moves":["Blizzard","Thunder Wave","Hyper Beam","Body Slam"]},
+		"3U Tank": {"level":100,"moves":["Body Slam","Reflect","Light Screen","Rest"]},
+		"2U Standard": {"level":100,"moves":["Blizzard","Body Slam","Thunder Wave","Thunderbolt"]},
+	},
+	{
+	"Cloyster": {
+		"1U Standard": {"level":100,"moves":["Blizzard","Clamp","Explosion","Hyper Beam"]},
+	},
+	{
+	"Dewgong": {
+		"3U Standard": {"level":100,"moves":["Ice Beam","Surf","Body Slam","Rest"]},
+	},
+	{
+	"Dodrio": {
+		"2U Sweeper": {"level":100,"moves":["Drill Peck","Body Slam","Hyper Beam","Agility"]},
+		"1U Sweeper": {"level":100,"moves":["Body Slam","Hyper Beam","Drill Peck","Substitute"]},
+	},
+	{
+	"Dragonair": {
+		"5U AgiliWrap": {"level":100,"moves":["Wrap","Thunderbolt","Agility","Blizzard"]},
+		"4U AgiliWrap": {"level":100,"moves":["Wrap","Agility","Thunderbolt","Blizzard"]},
+	},
+	{
+	"Drowzee": {
+		"Standard": {"level":100,"moves":["Psychic","Thunder Wave","Hypnosis","Seismic Toss"]},
+	},
+	{
+	"Dugtrio": {
+		"3U Sweeper": {"level":100,"moves":["Earthquake","Rock Slide","Slash","Substitute"]},
+	},
+	{
+	"Dragonite": {
+		"1U AgiliWrap": {"level":100,"moves":["Wrap","Agility","Surf","Hyper Beam"]},
+	},
+	{
+	"Electabuzz": {
+		"3U Sweeper": {"level":100,"moves":["Thunder Wave","Thunderbolt","Psychic","Seismic Toss"]},
+	},
+	{
+	"Electrode": {
+		"5U Standard": {"level":100,"moves":["Thunderbolt","Thunder Wave","Explosion","Hyper Beam"]},
+		"4U Standard": {"level":100,"moves":["Thunderbolt","Explosion","Screech","Hyper Beam"]},
+		"3U Standard": {"level":100,"moves":["Thunderbolt","Explosion","Take Down","Screech"]},
+	},
+	{
+	"Exeggcute": {
+		"3U Standard": {"level":100,"moves":["Sleep Powder","Psychic","Stun Spore","Explosion"]},
+	},
+	{
+	"Exeggutor": {
+		"1U Standard Utility": {"level":100,"moves":["Psychic","Explosion","Sleep Powder","Stun Spore"]},
+		"1U Defensive": {"level":100,"moves":["Psychic","Leech Seed","Rest","Stun Spore"]},
+	},
+	{
+	"Fearow": {
+		"3U Standard": {"level":100,"moves":["Drill Peck","Double-Edge","Hyper Beam","Mirror Move"]},
+	},
+	{
+	"Flareon": {
+		"3U Standard": {"level":100,"moves":["Fire Blast","Body Slam","Hyper Beam","Fire Spin"]},
+		"1U Breaker": {"level":100,"moves":["Fire Blast","Body Slam","Hyper Beam","Fire Spin"]},
+	},
+	{
+	"Gastly": {
+		"4U Standard": {"level":100,"moves":["Psychic","Thunderbolt","Hypnosis","Rest"]},
+		"3U Standard": {"level":100,"moves":["Night Shade","Thunderbolt","Hypnosis","Explosion"]},
+	},
+	{
+	"Gengar": {
+		"1U Standard": {"level":100,"moves":["Hypnosis","Explosion","Night Shade","Thunderbolt"]},
+	},
+	{
+	"Gyarados": {
+		"2U Wallbreaker": {"level":100,"moves":["Body Slam","Hyper Beam","Hydro Pump","Thunderbolt"]},
+	},
+	{
+	"Golduck": {
+		"3U Amnesia": {"level":100,"moves":["Amnesia","Surf","Ice Beam","Rest"]},
+	},
+	{
+	"Golem": {
+		"1U Standard": {"level":100,"moves":["Earthquake","Rock Slide","Body Slam","Explosion"]},
+	},
+	{
+	"Graveler": {
+		"4U Standard": {"level":100,"moves":["Earthquake","Rock Slide","Explosion","Body Slam"]},
+		"3U Standard": {"level":100,"moves":["Earthquake","Rock Slide","Body Slam","Explosion"]},
+	},
+	{
+	"Haunter": {
+		"2U Lead": {"level":100,"moves":["Hypnosis","Thunderbolt","Night Shade","Explosion"]},
+	},
+	{
+	"Hypno": {
+		"2U Status Spreader": {"level":100,"moves":["Psychic","Seismic Toss","Thunder Wave","Hypnosis"]},
+		"1U Lead / Support": {"level":100,"moves":["Hypnosis","Thunder Wave","Psychic","Rest"]},
+	},
+	{
+	"Jolteon": {
+		"1U Spark Plug": {"level":100,"moves":["Thunderbolt","Thunder Wave","Double kick","Pin Missile"]},
+	},
+	{
+	"Jynx": {
+		"1U Standard": {"level":100,"moves":["Lovely Kiss","Blizzard","Psychic","Rest"]},
+	},
+	{
+	"Kabutops": {
+	{
+		"6U Swords Dance": {"level":100,"moves":["Swords Dance","Surf","Hyper Beam","Slash"]},
+		"3U Swords Dance": {"level":100,"moves":["Swords Dance","Hyper Beam","Surf","Slash"]},
+	},
+	{
+	"Kadabra": {
+		"2U Standard": {"level":100,"moves":["Psychic","Seismic Toss","Thunder Wave","Recover"]},
+	},
+	{
+	"Kangaskhan": {
+		"2U Sweeper": {"level":100,"moves":["Body Slam","Hyper Beam","Earthquake","Surf"]},
+		"1U Standard": {"level":100,"moves":["Body Slam","Hyper Beam","Earthquake","Counter"]},
+	},
+	{
+	"Kingler": {
+		"3U Standard": {"level":100,"moves":["Body Slam","Crabhammer","Swords Dance","Hyper Beam"]},
+		"1U Swords Dance": {"level":100,"moves":["Swords Dance","Crabhammer","Body Slam","Hyper Beam"]},
+	},
+	{
+	"Lapras": {
+		"1U Rules of the Seas": {"level":100,"moves":["Blizzard","Thunderbolt","Body Slam","Sing"]},
+	},
+	{
+	"Lickitung": {
+		"5U Swords Dance": {"level":100,"moves":["Swords Dance","Hyper Beam","Earthquake","Body Slam"]},
+	},
+	{
+	"Machamp": {
+		"6U All-Out Attacker": {"level":100,"moves":["Earthquake","Rock Slide","Hyper Beam","Counter"]},
+		"4U Sweeper": {"level":100,"moves":["Submission","Earthquake","Hyper Beam","Fire Blast"]},
+	},
+	{
+	"Magmar": {
+		"5U Standard": {"level":100,"moves":["Fire Blast","Psychic","Body Slam","Seismic Toss"]},
+	},
+	{
+	"Magneton": {
+		"4U Standard": {"level":100,"moves":["Thunder","Thunderbolt","Thunder Wave","Double-Edge"]},
+	},
+	{
+	"Marowak": {
+		"6U Pivot": {"level":100,"moves":["Earthquake","Blizzard","Body Slam","Counter"]},
+	},
+	{
+	"Moltres": {
+		"1U Standard": {"level":100,"moves":["Fire Blast","Hyper Beam","Agility","Fire Spin"]},
+	},
+	{
+	"Mr. Mime": {
+		"3U Standard": {"level":100,"moves":["Psychic","Thunder Wave","Thunderbolt","Seismic Toss"]},
+	},
+	{
+	"Muk": {
+		"3U Explosion": {"level":100,"moves":["Explosion","Thunderbolt","Sludge","Hyper Beam"]},
+	},
+	{
+	"Nidoking": {
+		"4U Mixed Attacker": {"level":100,"moves":["Earthquake","Thunderbolt","Fire Blast","Substitute"]},
+		"3U Mixed Attacker": {"level":100,"moves":["Blizzard","Earthquake","Thunderbolt","Fire Blast"]},
+		"1U Standard": {"level":100,"moves":["Earthquake","Blizzard","Body Slam","Substitute"]},
+	},
+	{
+	"Nidoqueen": {
+		"4U Mixed Attacker": {"level":100,"moves":["Earthquake","Thunderbolt","Fire Blast","Substitute"]},
+		"1U Standard": {"level":100,"moves":["Earthquake","Blizzard","Body Slam","Substitute"]},
+	},
+	{
+	"Ninetales": {
+		"5U Mozilla Mascot": {"level":100,"moves":["Fire Blast","Fire Spin","Hyper Beam","Body Slam"]},
+		"4U Standard": {"level":100,"moves":["Fire Blast","Fire Spin","Body Slam","Hyper Beam"]},
+		"3U Standard": {"level":100,"moves":["Fire Blast","Fire Spin","Body Slam","Hyper Beam"]},
+	},
+	{
+	"Omastar": {
+		"4U Standard": {"level":100,"moves":["Hydro Pump","Blizzard","Seismic Toss","Rest"]},
+		"3U I'm A Star": {"level":100,"moves":["Hydro Pump","Blizzard","Seismic Toss","Rest"]},
+	},
+	{
+	"Persian": {
+		"2U Sweeper": {"level":100,"moves":["Slash","Hyper Beam","Bubble Beam","Thunderbolt"]},
+		"1U Sweeper / Revenge Killer": {"level":100,"moves":["Slash","Hyper Beam","Bubble Beam","Screech"]},
+	},
+	{
+	"Pidgeot": {
+		"5U Standard": {"level":100,"moves":["Double Edge","Hyper Beam","Wing Attack","Mirror Move"]},
+		"4U Bird Jesus": {"level":100,"moves":["Hyper Beam","Double-Edge","Mirror Move","Toxic"]},
+	},
+	{
+	"Pinsir": {
+		"3U Swords Dance": {"level":100,"moves":["Swords Dance","Bind","Body Slam","Hyper Beam"]},
+		"1U Swords Dance Sweeper": {"level":100,"moves":["Swords Dance","Slash","Submission","Hyper Beam"]},
+	},
+	{
+	"Poliwag": {
+		"5U Standard": {"level":100,"moves":["Hypnosis","Amnesia","Surf","Psychic"]},
+	},
+	{
+	"Poliwhirl": {
+		"3U Lead": {"level":100,"moves":["Hypnosis","Amnesia","Surf","Psychic"]},
+	},
+	{
+	"Poliwrath": {
+		"2U Pivot": {"level":100,"moves":["Hypnosis","Amnesia","Surf","Rest"]},
+	},
+	{
+	"Ponyta": {
+		"5U Standard": {"level":100,"moves":["Fire Blast","Fire Spin","Toxic","Agility"]},
+	},
+	{
+	"Porygon": {
+		"4U Standard": {"level":100,"moves":["Psychic","Thunderbolt","Thunder Wave","Recover"]},
+		"1U Cancerduck": {"level":100,"moves":["Ice Beam","Thunder Wave","Recover","Flash"]},
+	},
+	{
+	"Raichu": {
+		"2U Sweeper": {"level":100,"moves":["Thunder Wave","Thunderbolt","Surf","Hyper Beam"]},
+		"1U Standard": {"level":100,"moves":["Thunderbolt","Thunder Wave","Surf","Seismic Toss"]},
+	},
+	{
+	"Rapidash": {
+		"4U Standard": {"level":100,"moves":["Fire Blast","Fire Spin","Body Slam","Hyper Beam"]},
+	},
+	{
+	"Raticate":
+		"2U Wallbreaker": {"level":100,"moves":["Super Fang","Body Slam","Hyper Beam","Blizzard"]},
+		"1U Standard": {"level":100,"moves":["Super Fang","Hyper Beam","Body Slam","Bubble Beam"]},
+	},
+	{
+	"Rhydon": {
+		"2U Wallbreaker": {"level":100,"moves":["Earthquake","Rock Slide","Body Slam","Substitute"]},
+		"1U Standard": {"level":100,"moves":["Earthquake","Rock Slide","Body Slam","Substitute"]},
+	},
+	{
+	"Sandslash": {
+		"3U Swords Dance": {"level":100,"moves":["Swords Dance","Earthquake","Rock Slide","Body Slam"]},
+	},
+	{
+	"Scyther": {
+		"4U Swords Dance": {"level":100,"moves":["Slash","Swords Dance","Hyper Beam","Substitute"]},
+		"3U Swords Dance": {"level":100,"moves":["Swords Dance","Hyper Beam","Double Edge","Quick Attack"]},
+	},
+	{
+	"Seadra": {
+		"5U Pivot": {"level":100,"moves":["Hydro Pump","Ice Beam","Double-Edge","Rest"]},
+		"4U Standard": {"level":100,"moves":["Surf","Blizzard","Double Edge","Hydro Pump"]},
+	},
+	{
+	"Slowbro": {
+		"1U Tobybro": {"level":100,"moves":["Amnesia","Rest","Thunder Wave","Psychic"]},
+	},
+	{
+	"Slowpoke": {
+		"4U Tobypoke": {"level":100,"moves":["Amnesia","Rest","Thunder Wave","Psychic"]},
+	},
+	{
+	"Snorlax": {
+		"1U Standard Lax": {"level":100,"moves":["Body Slam","Earthquake","Hyper Beam","Self-Destruct"]},
+		"1U ReflectLax": {"level":100,"moves":["Body Slam","Reflect","Rest","Earthquake"]},
+		"1U AmneLax": {"level":100,"moves":["Amnesia","Ice Beam","Body Slam","Rest"]},
+	},
+	{
+	"Starmie": {
+		"1U The Star of the Show": {"level":100,"moves":["Recover","Thunder Wave","Blizzard","Thunderbolt"]},
+	},
+	{
+	"Staryu": {
+		"4U Standard": {"level":100,"moves":["Thunder Wave","Recover","Surf","Thunderbolt"]},
+	},
+	{
+	"Tangela": {
+		"4U Standard": {"level":100,"moves":["Sleep Powder","Stun Spore","Mega Drain","Growth"]},
+		"3U Standard": {"level":100,"moves":["Sleep Powder","Stun Spore","Mega Drain","Growth"]},
+	},
+	{
+	"Tauros": {
+		"1U Standard": {"level":100,"moves":["Body Slam","Hyper Beam","Earthquake","Blizzard"]},
+	},
+	{
+	"Tentacruel": {
+		"2U Swords Dance Sweeper": {"level":100,"moves":["Swords Dance","Hydro Pump","Wrap","Hyper Beam"]},
+	},
+	{
+	"Vaporeon": {
+		"3U Standard": {"level":100,"moves":["Surf","Ice Beam","Body Slam","Rest"]},
+	},
+	{
+	"Venomoth": {
+		"4U Standard": {"level":100,"moves":["Sleep Powder","Stun Spore","Psychic","Double Edge"],
+		"3U Sleeper": {"level":100,"moves":["Sleep Powder","Stun Spore","Psychic","Double-Edge"]},
+	},
+	{
+	"Venusaur": {
+		"3U Standard": {"level":100,"moves":["Sleep Powder","Razor Leaf","Body Slam","Hyper Beam"]},
+		"1U Standard": {"level":100,"moves":["Razor Leaf","Sleep Powder","Swords Dance","Hyper Beam"]},
+	},
+	{
+	"Victreebel": {
+	{
+		"2U Status Spreader": {"level":100,"moves":["Sleep Powder","Razor Leaf","Wrap","Stun Spore"]},
+		"2U Swords Dance Sweeper": {"level":100,"moves":["Swords Dance","Razor Leaf","Hyper Beam","Sleep Powder"]},
+		"1U Wrap": {"level":100,"moves":["Sleep Powder","Wrap","Razor Leaf","Stun Spore"]},
+		"1U Swords Dance": {"level":100,"moves":["Swords Dance","Hyper Beam","Razor Leaf","Sleep Powder"]},
+	},
+	{
+	"Vileplume": {
+		"Status Spreader": {"level":100,"moves":["Sleep Powder","Stun Spore","Mega Drain","Body Slam"]},
+	},
+	{
+	"Wartortle": {
+		"5U Standard": {"level":100,"moves":["Hydro Pump","Ice Beam","Seismic Toss","Rest"]},
+	},
+	{
+	"Weepinbell": {
+		"6U Status Spreader": {"level":100,"moves":["Sleep Powder","Stun Spore","Razor Leaf","Wrap"]},
+		"4U Standard": {"level":100,"moves":["Sleep Powder","Stun Spore","Razor Leaf","Wrap"]},
+	},
+	{
+	"Weezing": {
+		"3U Explosion": {"level":100,"moves":["Explosion","Thunderbolt","Sludge","Fire Blast"]},
+	},
+	{
+	"Wigglytuff": {
+		"3U Standard": {"level":100,"moves":["Blizzard","Thunder Wave","Hyper Beam","Body Slam"]},
+	},
+	{
+	"Zapdos": {
+		"1U Sweeper": {"level":100,"moves":["Thunderbolt","Drill Peck","Thunder Wave","Agility"]},
+	},
+};
