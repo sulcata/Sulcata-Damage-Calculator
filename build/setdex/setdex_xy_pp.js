53f198d6e3faf63d97d7ed48effcd65f477408c7
DisasterAreaPS <gsalomon@zoho.com>
2017-06-20 22:52:43 +0100

    Create setdex_xy_pp.js (#10)

---
+++ build/setdex/setdex_xy_pp.js
@@ -0,0 +1,107 @@
+export default {
+	"Arceus": {
+		"1U Extreme Speed Killer (Ekiller)": {"level":100,"evs":{"at":252,"df":4,"sp":252},"nature":"Jolly","ability":"Multitype","item":"Life Orb","moves":["Extreme Speed","Swords Dance","Shadow Claw","Earthquake"]},
+	},
+	"Arceus-Electric": {
+		"1U Calm Mind": {"level":100,"evs":{"hp":4,"sa":252,"sp":252},"nature":"Timid","ability":"Multitype","item":"Zap Plate","moves":["Calm Mind","Judgment","Ice Beam","Recover"]},
+	},
+	"Arceus-Ghost": {
+		"1U Swords Dance": {"level":100,"evs":{"at":252,"df":4,"sp":252},"nature":"Jolly","ability":"Multitype","item":"Spooky Plate","moves":["Swords Dance","Shadow Force","Brick Break","Extreme Speed"]},
+		"1U Calm Mind": {"level":100,"evs":{"sa":252,"sd":4,"sp":252},"nature":"Timid","ability":"Multitype","item":"Spooky Plate","moves":["Calm Mind","Judgment","Recover","Focus Blast"]},
+		"1U Support": {"level":100,"evs":{"hp":252,"df":4,"sp":252},"nature":"Timid","ability":"Multitype","item":"Spooky Plate","moves":["Recover","Defog","Will-O-Wisp","Judgment"]},
+	},
+	"Arceus-Grass": {
+		"1U Support": {"level":100,"evs":{"hp":248,"sd":8,"sp":252},"nature":"Timid","ability":"Multitype","item":"Meadow Plate","moves":["Recover","Defog","Judgment","Will-O-Wisp"]},
+	},
+	"Arceus-Rock": {
+		"1U Support": {"level":100,"evs":{"hp":252,"df":4,"sp":252},"nature":"Timid","ability":"Multitype","item":"Stone Plate","moves":["Judgment","Recover","Defog","Will-O-Wisp"]},
+	},
+	"Blaziken": {
+		"1U Standard": {"level":100,"evs":{"at":252,"df":4,"sp":252},"nature":"Adamant","ability":"Speed Boost","item":"Blazikenite","moves":["Flare Blitz","Low Kick","Protect","Knock Off"]},
+	},
+	"Blaziken-Mega": {
+		"1U Standard": {"level":100,"evs":{"at":252,"df":4,"sp":252},"nature":"Adamant","ability":"Speed Boost","item":"Blazikenite","moves":["Flare Blitz","Low Kick","Protect","Knock Off"]},
+	},
+	"Darkrai": {
+		"1U Standard": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Bad Dreams","item":"Life Orb","moves":["Dark Void","Dark Pulse","Nasty Plot","Sludge Bomb"]},
+		"1U Substitute": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Bad Dreams","item":"Leftovers","moves":["Dark Void","Dark Pulse","Substitute","Nasty Plot"]},
+	},
+	"Dialga": {
+		"1U Bulky Stealth Rock": {"level":100,"evs":{"hp":248,"sa":8,"sd":252},"nature":"Calm","ability":"Pressure","item":"Leftovers","moves":["Stealth Rock","Draco Meteor","Flash Cannon","Toxic"]},
+	},
+	"Deoxys-Speed": {
+		"1U Suicide Lead": {"level":100,"evs":{"hp":252,"df":4,"sp":252},"nature":"Timid","ability":"Pressure","item":"Focus Sash","moves":["Stealth Rock","Taunt","Skill Swap","Spikes"]},
+	},
+	"Groudon": {
+		"1U Support": {"level":100,"evs":{"hp":248,"at":88,"sd":172},"nature":"Adamant","ability":"Drought","item":"Earth Plate","moves":["Stealth Rock","Precipice Blades","Stone Edge","Thunder Wave"]},
+		"1U ParaDancer": {"level":100,"evs":{"hp":248,"at":252,"sd":8},"nature":"Adamant","ability":"Drought","item":"Earth Plate","moves":["Thunder Wave","Precipice Blades","Stone Edge","Swords Dance"]},
+		"1U Rock Polish": {"level":100,"evs":{"hp":140,"at":252,"sp":116},"nature":"Adamant","ability":"Drought","item":"Life Orb","moves":["Rock Polish","Precipice Blades","Stone Edge","Fire Punch"]},
+		"1U Double Dance": {"level":100,"evs":{"hp":140,"at":252,"sp":116},"nature":"Adamant","ability":"Drought","item":"Lum Berry","moves":["Rock Polish","Precipice Blades","Stone Edge","Swords Dance"]},
+	},
+	"Ho-Oh": {
+		"1U Choice Band": {"level":100,"evs":{"hp":248,"at":244,"sd":16},"nature":"Adamant","ability":"Regenerator","item":"Choice Band","moves":["Brave Bird","Sacred Fire","Earthquake","Sleep Talk"]},
+		"1U Life Orb": {"level":100,"evs":{"hp":248,"at":244,"sd":16},"nature":"Adamant","ability":"Regenerator","item":"Life Orb","moves":["Sacred Fire","Brave Bird","Earthquake","Roost"]},
+		"1U Physically Defensive": {"level":100,"evs":{"hp":248,"df":244,"sd":16},"nature":"Impish","ability":"Regenerator","item":"Leftovers","moves":["Sacred Fire","Brave Bird","Roost","Thunder Wave"]},
+	},
+	"Kangaskhan": {
+		"1U Priority Abuser": {"level":100,"evs":{"at":252,"sd":4,"sp":252},"nature":"Jolly","ability":"Scrappy","item":"Kangaskhanite","moves":["Fake Out","Return","Earthquake","Sucker Punch"]},
+		"1U Power Up Punch": {"level":100,"evs":{"at":252,"sd":4,"sp":252},"nature":"Jolly","ability":"Scrappy","item":"Kangaskhanite","moves":["Power-Up Punch","Return","Earthquake","Crunch"]},
+		"1U Wish": {"level":100,"evs":{"hp":252,"df":4,"sd":252},"nature":"Calm","ability":"Scrappy","item":"Kangaskhanite","moves":["Wish","Protect","Toxic","Seismic Toss"]},
+	},
+	"Kangaskhan-Mega": {
+		"1U Priority Abuser": {"level":100,"evs":{"at":252,"sd":4,"sp":252},"nature":"Jolly","ability":"Parental Bond","item":"Kangaskhanite","moves":["Fake Out","Return","Earthquake","Sucker Punch"]},
+		"1U Power Up Punch": {"level":100,"evs":{"at":252,"sd":4,"sp":252},"nature":"Jolly","ability":"Parental Bond","item":"Kangaskhanite","moves":["Power-Up Punch","Return","Earthquake","Crunch"]},
+		"1U Wish": {"level":100,"evs":{"hp":252,"df":4,"sd":252},"nature":"Calm","ability":"Parental Bond","item":"Kangaskhanite","moves":["Wish","Protect","Toxic","Seismic Toss"]},
+	},
+	"Klefki": {
+		"1U Spiker": {"level":100,"evs":{"hp":252,"at":4,"sd":252},"nature":"Careful","ability":"Prankster","item":"Leftovers","moves":["Spikes","Thunder Wave","Toxic","Play Rough"]},
+	},
+	"Kyogre": {
+		"1U Choice Scarf": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Modest","ability":"Drizzle","item":"Choice Scarf","moves":["Water Spout","Thunder","Ice Beam","Origin Pulse"]},
+		"1U Choice Specs": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Drizzle","item":"Choice Specs","moves":["Water Spout","Thunder","Ice Beam","Origin Pulse"]},
+		"1U Bulky Twave": {"level":100,"evs":{"hp":252,"df":176,"sp":80},"nature":"Bold","ability":"Drizzle","item":"Leftovers","moves":["Thunder Wave","Surf","Thunder","Ice Beam"]},
+		"1U Calm Mind": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Drizzle","item":"Leftovers","moves":["Calm Mind","Origin Pulse","Ice Beam","Thunder"]},
+		"1U Specially Defensive": {"level":100,"evs":{"hp":252,"df":4,"sd":252},"nature":"Calm","ability":"Drizzle","item":"Leftovers","moves":["Rest","Sleep Talk","Roar","Scald"]},
+	},
+	"Mewtwo": {
+		"1U Offensive": {"level":100,"evs":{"sa":252,"sd":4,"sp":252},"nature":"Timid","ability":"Unnerve","item":"Life Orb","moves":["Psystrike","Fire Blast","Focus Blast","Ice Beam"]},
+		"1U Stalltwo": {"level":100,"evs":{"hp":252,"df":80,"sp":176},"nature":"Timid","ability":"Unnerve","item":"Leftovers","moves":["Will-O-Wisp","Recover","Taunt","Psystrike"]},
+	},
+	"Mewtwo-Mega-Y": {
+		"1U Offensive": {"level":100,"evs":{"sa":252,"sd":4,"sp":252},"nature":"Timid","ability":"Insomnia","item":"Mewtwonite Y","moves":["Psystrike","Fire Blast","Focus Blast","Ice Beam"]},
+	},
+	"Palkia": {
+		"1U Offensive": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Pressure","item":"Lustrous Orb","moves":["Spacial Rend","Hydro Pump","Fire Blast","Thunder Wave"]},
+		"1U Specially Defensive": {"level":100,"evs":{"hp":248,"sd":252,"sp":8},"nature":"Calm","ability":"Pressure","item":"Leftovers","moves":["Rest","Sleep Talk","Dragon Tail","Surf"]},
+		"1U Choice Scarf": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Modest","ability":"Pressure","item":"Choice Scarf","moves":["Spacial Rend","Hydro Pump","Fire Blast","Sleep Talk"]},
+		"1U Choice Specs": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Pressure","item":"Choice Specs","moves":["Spacial Rend","Hydro Pump","Draco Meteor","Fire Blast"]},
+		"1U Trick Room": {"level":100,"evs":{"hp":248,"sa":252,"sd":8},"ivs":{"sp":0}"nature":"Quiet","ability":"Pressure","item":"Lustrous Orb","moves":["Trick Room","Spacial Rend","Hydro Pump","Fire Blast"]},
+	},
+	"Rayquaza": {
+		"1U MixQuaza": {"level":100,"evs":{"at":252,"sa":40,"sp":216},"nature":"Naive","ability":"Air Lock","item":"Life Orb","moves":["Dragon Ascent","Earthquake","Draco Meteor","Extreme Speed"]},
+		"1U Dance Dance Rayvolution": {"level":100,"evs":{"hp":4,"at":252,"sp":252},"nature":"Adamant","ability":"Air Lock","item":"Life Orb","moves":["Swords Dance","Dragon Ascent","Earthquake","Extreme Speed"]},
+		"1U Choice Band": {"level":100,"evs":{"at":252,"sd":4,"sp":252},"nature":"Adamant","ability":"Air Lock","item":"Choice Band","moves":["Dragon Ascent","Earthquake","Extreme Speed","V-Create"]},
+	},
+	"Sableye": {
+		"1U Support": {"level":100,"evs":{"hp":248,"df":152,"sd":108},"nature":"Calm","ability":"Prankster","item":"Sablenite","moves":["Recover","Foul Play","Will-O-Wisp","Taunt"]},
+	},
+	"Sableye-Mega": {
+		"1U Support": {"level":100,"evs":{"hp":248,"df":152,"sd":108},"nature":"Calm","ability":"Magic Bounce","item":"Sablenite","moves":["Recover","Foul Play","Will-O-Wisp","Taunt"]},
+	},
+	"Xerneas": {
+		"1U Offensice Calm Mind": {"level":100,"evs":{"df":32,"sa":252,"sp":224},"nature":"Timid","ability":"Fairy Aura","item":"Life Orb","moves":["Moonblast","Calm Mind","Thunder","Focus Blast"]},
+		"1U All-Out Attacker (Mixed)": {"level":100,"evs":{"at":84,"sa":200,"sp":224},"nature":"Mild","ability":"Fairy Aura","item":"Life Orb","moves":["Moonblast","Rock Slide","Close Combat","Hidden Power Fire"]},
+		"1U Offensive Aromatherapy": {"level":100,"evs":{"df":32,"sa":252,"sp":224},"nature":"Timid","ability":"Fairy Aura","item":"Leftovers","moves":["Moonblast","Aromatherapy","Thunder","Focus Blast"]},
+		"1U Choice Scarf": {"level":100,"evs":{"df":32,"sa":252,"sp":224},"nature":"Modest","ability":"Fairy Aura","item":"Choice Scarf","moves":["Moonblast","Sleep Talk","Focus Blast","Thunder"]},
+		"1U RestTalk": {"level":100,"evs":{"hp":248,"df":40,"sd":216},"nature":"Calm","ability":"Fairy Aura","item":"Leftovers","moves":["Rest","Sleep Talk","Moonblast","Calm Mind"]},
+	},
+	"Yveltal": {
+		"1U Fashionable Bird (Choice Scarf)": {"level":100,"evs":{"hp":32,"df":4,"sa":248,"sp":224},"nature":"Modest","ability":"Dark Aura","item":"Choice Scarf","moves":["Dark Pulse","Oblivion Wing","Foul Play","U-Turn"]},
+		"1U Strong Bird": {"level":100,"evs":{"hp":32,"at":128,"sa":252,"sp":96},"nature":"Rash","ability":"Dark Aura","item":"Life Orb","moves":["Dark Pulse","Oblivion Wing","Sucker Punch","Heat Wave"]},
+		"1U Tough Bird (Physical Tank)": {"level":100,"evs":{"hp":248,"df":252,"sp":8},"nature":"Impish","ability":"Dark Aura","item":"Black Glasses","moves":["Roost","Foul Play","Toxic","Sucker Punch"]},
+	},
+	"Zekrom": {
+		"1U Choice Scarf": {"level":100,"evs":{"at":252,"sa":4,"sp":252},"nature":"Naughty","ability":"Teravolt","item":"Choice Scarf","moves":["Bolt Strike","Outrage","Draco Meteor","Volt Switch"]},
+		"1U Life Orb": {"level":100,"evs":{"at":252,"sa":4,"sp":252},"nature":"Naughty","ability":"Teravolt","item":"Life Orb","moves":["Bolt Strike","Draco Meteor","Outrage","Tailwind"]},
+	},
+};
