4b32577da4d377d1cd2a615b145a2b1ec483fd48
sulcata <sulcata@users.noreply.github.com>
2018-10-11 23:18:52 -0400

    fix(*): remove reference to missing babel plugin

--- babel.config.js
+++ babel.config.js
@@ -18,7 +18,6 @@ module.exports = function(api) {
 
   const plugins = [
     ["@babel/plugin-proposal-nullish-coalescing-operator", { loose: true }],
-    "@babel/plugin-proposal-numeric-separator",
     "@babel/plugin-proposal-export-default-from",
     "@babel/plugin-proposal-export-namespace-from",
     "lodash",
