8b940bc17a6fb9de78a2cbfad931920876791e48
sulcata <sulcata@users.noreply.github.com>
2018-10-13 01:30:04 -0400

    fix(browsers): attempt to fix issue with travis build by excluding ie 11

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = function(api) {
       "@babel/preset-env",
       {
         targets: {
-          browsers: ">= 0.1%, last 1 version, not dead, current node"
+          browsers: ">= 0.1%, last 1 version, not ie 11, not dead, current node"
         },
         modules: api.env("webpack") ? false : "commonjs",
         shippedProposals: true,
