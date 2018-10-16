cbec3f26811dc9a2b96002a6979a457758c110b6
sulcata <sulcata@users.noreply.github.com>
2018-10-16 08:11:35 -0400

    refactor(app): move vuex plugin install to main entry point

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "vue-multiselect/dist/vue-multiselect.min.css";
 import Vue from "vue";
+import Vuex from "vuex";
 import App from "./components/App.vue";
 import store from "./store";
+
+Vue.use(Vuex);
 
 const vm = new Vue({
   render: h => h(App),
   store
-}).$mount();
+});
 
-document.body.appendChild(vm.$el);
+document.body.appendChild(vm.$mount().$el);
--- app/store/index.js
+++ app/store/index.js
@@ -1,11 +1,8 @@
-import Vue from "vue";
-import Vuex, { Store } from "vuex";
+import { Store } from "vuex";
 import state from "./state";
 import * as mutations from "./mutations";
 import * as getters from "./getters";
 import { persistencePlugin } from "./plugins";
-
-Vue.use(Vuex);
 
 export default new Store({
   strict: process.env.NODE_ENV !== "production",
