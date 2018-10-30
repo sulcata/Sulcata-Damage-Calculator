83287be18b16f4148b67f9d7a1937d6ac331adba
sulcata <sulcata@users.noreply.github.com>
2018-10-30 01:18:40 -0400

    chore(*): split jest.config.js to use projects for src and app
    -specify test environments for each project
    -split coverage thresholds
    -ensure coverage is collected for all files

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,51 @@
 "use strict";
 
 module.exports = {
-  coveragePathIgnorePatterns: ["/node_modules/", "/test/", "/dist/"],
+  collectCoverageFrom: [
+    "<rootDir>/app/**/*.{js,vue}",
+    "<rootDir>/src/**/*.{js,vue}",
+    "!**/node_modules/**",
+    "!**/dist/**",
+    "!**/*.test.*"
+  ],
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
   coverageThreshold: {
-    global: {
+    "./src": {
       branches: 55,
       functions: 95,
-      statements: 60
+      statements: 65
+    },
+    "./app": {
+      branches: 50,
+      functions: 55,
+      statements: 50
     }
   },
-  moduleFileExtensions: ["js", "json", "vue"],
-  moduleNameMapper: {
-    "^sulcalc$": "<rootDir>/src/index",
-    "^sulcalc/(.*)$": "<rootDir>/src/$1",
-    "^app$": "<rootDir>/app/index",
-    "^app/(.*)$": "<rootDir>/app/$1",
-    "^package$": "<rootDir>/package.json"
-  },
-  transform: {
-    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
-    "^.+\\.vue$": "<rootDir>/node_modules/vue-jest"
-  }
+  projects: [
+    {
+      displayName: "sulcalc",
+      testEnvironment: "node",
+      testMatch: ["<rootDir>/src/**/*.(spec|test).js?(x)"],
+      moduleNameMapper: {
+        "^sulcalc$": "<rootDir>/src/index",
+        "^sulcalc/(.*)$": "<rootDir>/src/$1",
+        "^package$": "<rootDir>/package.json"
+      }
+    },
+    {
+      displayName: "app",
+      testEnvironment: "jsdom",
+      testMatch: ["<rootDir>/app/**/*.(spec|test).js?(x)"],
+      moduleFileExtensions: ["js", "json", "vue"],
+      moduleNameMapper: {
+        "^sulcalc$": "<rootDir>/src/index",
+        "^sulcalc/(.*)$": "<rootDir>/src/$1",
+        "^package$": "<rootDir>/package.json"
+      },
+      transform: {
+        "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+        "^.+\\.vue$": "<rootDir>/node_modules/vue-jest"
+      }
+    }
+  ]
 };
